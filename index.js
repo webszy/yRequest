232840ee503e8715acb966af57a4d9a4650a2d56
孙振宇 <sunzhenyu@live.com>
2020-11-03 13:51:59 +0800

    update: output a get

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const axios = require('axios')
 const axiosRetry = require('axios-retry')
 const to = require('await-to-js').default
+function isObject(obj) {
+  return obj !== null && typeof obj === 'object'
+}
+function isFunction(obj) {
+  return obj !== null && typeof obj === 'function'
+}
+function isArray(arr) {
+  return obj !== null && typeof obj === 'object' && arr.length
+}
+const defaultParam = {
+  requestOption: {
+    timeoout:7* 1000
+  },
+  interceptors:{},
+  errorHandlers:{},
+  retryOption:{
+    enable:true
+  },
+  allowMethods:['get','post','put','delete']
+}
 /**
  *
  *
@@ -10,25 +30,71 @@ const to = require('await-to-js').default
  * interceptors：{//拦截器配置，3个函数
  *  beforeRequest:Function，
  *  beforeResponse:Function，
- *  globalErrorHandler:Function
+ *  responseError:Function
  * }, 
+ * allowMethods:Array of method name ,like get post etc
  * errorHandlers:Map, //处理具体请求失败,基于url path处理
  * retryOption:{
  *  enable: boolean,
- *  times:Nmber
+ *  delay:Number,
+ *  retries:number
  * }
  * }
  */
 class Request {
-  constructor({ requestOption,interceptors, errorHandlers, retryOption }) {
-    if(requestOption){
+  constructor({ requestOption, interceptors, errorHandlers, retryOption,allowMethods } = defaultParam ) {
+    if(isArray(allowMethods)){
+      this.allowMethods = allowMethods
+    } else {
+      this.allowMethods = defaultParam.allowMethods
+    }
+    
+    if (isObject(requestOption)) {
       this.Service = axios.create(requestOption)
     } else {
       this.Service = axios.create()
     }
-    if(interceptors){
-      if(interceptors.beforeRequest&& typeof interceptors.beforeRequest === 'function')
+    if (isFunction(interceptors.beforeRequest)) {
       this.Service.interceptors.request.use(interceptors.beforeRequest)
+    }
+    let beforeRequest = null
+    if (isFunction(interceptors.beforeResponse)) {
+      beforeRequest = interceptors.beforeRequest
+    } else {
+      beforeRequest = response => response.data
+    }
+    let responseError = null
+    if (isFunction(interceptors.responseError)) {
+      responseError = interceptors.responseError
+    } else {
+      responseError = error => console.log('request error', error)
+    }
+    this.Service.interceptors.response.use(beforeRequest, responseError)
+    if (retryOption && retryOption.enable) {
+      axiosRetry(this.Service, {
+        retries: retryOption.retries || 2,
+        retryDelay: delayCount => delayCount * (retryOption.delay || 200)
+      })
+    }
+    return this.output()
+  }
+  output() {
+    const _get = (url, header, param) => {
+      const p = { method: 'get', url }
+      if (isObject(header)) {
+        p.headers = header
+      }
+      if (isObject(param)) {
+        p.param = param
+      }
+      this.Service(p)
+    }
+    return {
+      _get,
+      Service: this.Service
     }
   }
 }
+
+const s = new Request()
+console.log('s: ', s);
