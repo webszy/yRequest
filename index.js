7b772c6cb93e22c12c70ef124ee001dffb62251b
孙振宇 <sunzhenyu@live.com>
2020-11-06 17:37:35 +0800

    cannot use string to make functions

--- index.js
+++ index.js
@@ -84,29 +84,50 @@ class Request {
     const funcList = {
       Service: this.Service
     }
-    for (const method of this.allowMethods) {
-      const fun_body = "function isObject(obj) {" +
-        "return (obj !== null && typeof obj === 'object') || typeof obj === 'undefined'" +
-        "};" +
-        "const str = new URL(url);" +
-        "const path = str.pathname;" +
-        "const p = {method:" + method + "};" +
-        "if(!url || url===''){ throw new Error('must has url') };" +
-        "if(!isObject(header)){ throw new Error('header must be Object') } else { p.headers = header };" +
-        "if(!isObject(param)){ throw new Error('param must be Object') } else { p.param = param };" +
-        "if(isObject(data)&&p.method.toLowerCase()!=='get'){ p.data=data };" +
-        "const [err,res] = this.to(this.Service(p));" +
-        "if (!err && res) {" +
-        " return res" +
-        "} else {" +
-        " console.log(err);" +
-        " if(this.errorHandlers&&this.errorHandlers.has(path)){" +
-        "  const func = this.errorHandlers.get(path);" +
-        "  func(err);" +
-        "} }"
-      console.log('fun_body: ', fun_body);
-      const name = '_' + method
-      funcList[name] = new Function('url', 'header', 'param', 'data', fun_body).bind(this)
+    // for (const method of this.allowMethods) {
+    //   const fun_body = "function isObject(obj) {" +
+    //     "return (obj !== null && typeof obj === 'object') || typeof obj === 'undefined'" +
+    //     "};" +
+    //     "const str = new URL(url);" +
+    //     "const path = str.pathname;" +
+    //     "const p = {method:" + method + "};" +
+    //     "if(!url || url===''){ throw new Error('must has url') };" +
+    //     "if(!isObject(header)){ throw new Error('header must be Object') } else { p.headers = header };" +
+    //     "if(!isObject(param)){ throw new Error('param must be Object') } else { p.param = param };" +
+    //     "if(isObject(data)&&p.method.toLowerCase()!=='get'){ p.data=data };" +
+    //     "const [err,res] = -this.to(this.Service(p));" +
+    //     "if (!err && res) {" +
+    //     " return res" +
+    //     "} else {" +
+    //     " console.log(err);" +
+    //     " if(this.errorHandlers&&this.errorHandlers.has(path)){" +
+    //     "  const func = this.errorHandlers.get(path);" +
+    //     "  func(err);" +
+    //     "} }"
+    //   console.log('fun_body: ', fun_body);
+    //   const name = '_' + method
+    //   funcList[name] = new Function('url', 'header', 'param', 'data', fun_body).bind(this)
+    // }
+    funcList._get = async (url, header, param) => {
+      function isObject(obj) {
+        return (obj !== null && typeof obj === 'object') || typeof obj === 'undefined'
+      };
+      const str = new URL(url);
+      const path = str.pathname;
+      const p = { method: " + method + " };
+      if (!url || url === '') { throw new Error('must has url') };
+      if (!isObject(header)) { throw new Error('header must be Object') } else { p.headers = header };
+      if (!isObject(param)) { throw new Error('param must be Object') } else { p.param = param };
+      if (isObject(data) && p.method.toLowerCase() !== 'get') { p.data = data };
+      const [err, res] = this.to(this.Service(p));
+      if (!err && res) {
+      } else {
+        console.log(err)
+        if (this.errorHandlers && this.errorHandlers.has(path)) {
+          const func = this.errorHandlers.get(path);
+          func(err);
+        }
+      }
     }
     return funcList
   }
